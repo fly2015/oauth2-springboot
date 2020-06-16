f737e6b2ba48e129189507b857a9b103549ec362
nhqhien <nhqhien@DN-AAVN-WS-009.aavn.local>
2020-06-16 17:01:27 +0700

    initial commit

---
+++ src/main/com/oauth2/AuthServerOAuth2Config.java
@@ -0,0 +1,25 @@
+/*
+ * AuthServerOAuth2Config.java
+ *
+ * Copyright by CRIF AG
+ * Zürich
+ * All rights reserved.
+ */
+package com.oauth2;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+@Configurationon
+@EnableAuthorizationServererve
+public class AuthServerOAuth2Config extends AuthorizationServerConfigurerAdapter {
+
+}
+
+
+
+/*
+ * Changes:
+ * $Log: $
+ */
